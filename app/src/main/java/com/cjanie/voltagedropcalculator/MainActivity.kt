2e131a8f430abfbd181c8629f288f07ef63584ae
cjanie <janiechun@hotmail.fr>
2024-10-05 16:51:35 +0200

    ui componants

--- app/src/main/java/com/cjanie/voltagedropcalculator/MainActivity.kt
+++ app/src/main/java/com/cjanie/voltagedropcalculator/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -20,10 +26,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cjanie.voltagedropcalculator.ui.theme.Typography
 import com.cjanie.voltagedropcalculator.ui.theme.VoltageDropCalculatorTheme
+import com.cjanie.voltagedropcalculator.ui.theme.copperColor
+import com.cjanie.voltagedropcalculator.ui.theme.onCopperColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,55 +50,55 @@ class MainActivity : ComponentActivity() {
         setContent {
             VoltageDropCalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier
-                        .padding(innerPadding)) {
-                        Title(
-                            name = getString(R.string.app_name)
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Header(
+                            text = getString(R.string.app_name)
                         )
 
                         Column {
 
-                            Label(name = formPresenter.functionalContextLabel)
-                            Dropdown(
+                            LabeledDropdown(
+                                name = formPresenter.functionalContextLabel,
                                 options = formPresenter.functionalContexts,
                                 select =  fun(index: Int) {
                                     calculatorModel.setFunctionnalContext(index)
                                 }
                             )
-                            
-                            Label(name = formPresenter.lineTypeLabel)
-                            Dropdown(
+
+                            LabeledDropdown(
+                                name = formPresenter.lineTypeLabel,
                                 options = formPresenter.lineTypeOptions,
                                 select =  fun(index: Int) {
                                     calculatorModel.setLineType(index)
                                 }
                             )
-                            
-                            Label(name = formPresenter.conductorLabel)
-                            Dropdown(options = formPresenter.conductorOptions,
+
+                            LabeledDropdown(
+                                name = formPresenter.conductorLabel,
+                                options = formPresenter.conductorOptions,
                                 select =  fun(index: Int) {
                                     calculatorModel.setConductor(index)
                                 }
                             )
 
-                            Label(name = formPresenter.sectionLabel)
-                            Dropdown(
+                            LabeledDropdown(
+                                name = formPresenter.sectionLabel,
                                 options = formPresenter.sectionOptions,
                                 select =  fun(index: Int) {
                                     calculatorModel.setSection(index)
                                 }
                             )
                             
-                            Label(name = formPresenter.currentIntensityLabel)
-                            Dropdown(
+                            LabeledDropdown(
+                                name = formPresenter.currentIntensityLabel,
                                 options = formPresenter.currentIntensityOptions,
                                 select =  fun(index: Int) {
                                     calculatorModel.setIntensity(index)
                                 }
                             )
                             
-                            Label(name = formPresenter.tensionLabel)
-                            Dropdown(
+                            LabeledDropdown(
+                                name = formPresenter.currentIntensityLabel,
                                 options = formPresenter.tensionOptions,
                                 select =  fun(index: Int) {
                                     calculatorModel.setTension(index)
@@ -152,16 +165,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Title(name: String, modifier: Modifier = Modifier) {
+fun Header(text: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = copperColor)
+            .padding(20.dp),
+        horizontalArrangement = Arrangement.Center
+        ) {
+        Title(name = text, textColor = onCopperColor)
+    }
+}
+
+@Composable
+fun Title(name: String, textColor: Color) {
+    val textStyle = Typography.titleLarge
     Text(
         text = name,
-        modifier = modifier
+        fontFamily = textStyle.fontFamily,
+        fontWeight = textStyle.fontWeight,
+        fontSize = textStyle.fontSize,
+        lineHeight = textStyle.lineHeight,
+        letterSpacing = textStyle.letterSpacing,
+        color = textColor
     )
 }
 
 @Composable
+fun LabeledDropdown(name: String, options: Array<String>, select: (itemPosition: Int) -> Unit) {
+    Column(modifier = Modifier.padding(8.dp)) {
+        Label(name = name)
+        Dropdown(options = options, select)
+    }
+}
+
+@Composable
 fun Label(name: String) {
-    Text(text = name)
+    val textStyle: TextStyle = Typography.labelLarge
+    Text(
+        text = name,
+        fontFamily = textStyle.fontFamily,
+        fontWeight = textStyle.fontWeight,
+        fontSize = textStyle.fontSize,
+        lineHeight = textStyle.lineHeight,
+        letterSpacing = textStyle.letterSpacing
+    )
 }
 
 
@@ -175,9 +223,18 @@ fun Dropdown(options: Array<String>, select: (itemPosition: Int) -> Unit) {
     var itemPosition by remember {
         mutableStateOf(0)
     }
+
     select(itemPosition)
 
-    Button(onClick = { isDropDownExpanded = true }) {
+    Button(
+        onClick = { isDropDownExpanded = true },
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonColors(
+            containerColor = copperColor,
+            contentColor = onCopperColor,
+            disabledContainerColor = copperColor,
+            disabledContentColor = onCopperColor)
+    ) {
         Text(text = options[itemPosition])
     }
     DropdownMenu(
@@ -216,6 +273,6 @@ fun NumberInput(select: (value: Float) -> Unit) {
 @Composable
 fun GreetingPreview() {
     VoltageDropCalculatorTheme {
-        Title("Android")
+
     }
 }
--- app/src/main/java/com/cjanie/voltagedropcalculator/ui/theme/Color.kt
+++ app/src/main/java/com/cjanie/voltagedropcalculator/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val copperColor = Color(0xFFB77729)
+val onCopperColor = Color.White
