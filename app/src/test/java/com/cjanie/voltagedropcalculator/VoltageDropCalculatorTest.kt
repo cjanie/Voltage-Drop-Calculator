3d36834cfdb41d9f5e42ba1e3db635d941be4e29
cjanie <janiechun@hotmail.fr>
2025-01-02 16:38:34 +0100

    voltage drop calculator test

--- app/src/test/java/com/cjanie/voltagedropcalculator/VoltageDropCalculatorTest.kt
+++ app/src/test/java/com/cjanie/voltagedropcalculator/VoltageDropCalculatorTest.kt
@@ -3,15 +3,14 @@ package com.cjanie.voltagedropcalculator
 import com.cjanie.voltagedropcalculator.businesslogic.enums.ConductorMaterial
 import com.cjanie.voltagedropcalculator.businesslogic.enums.Usage
 import com.cjanie.voltagedropcalculator.businesslogic.valueobjects.Intensity
+import com.cjanie.voltagedropcalculator.businesslogic.voltagedrop.VoltageDropCalculator
 import com.cjanie.voltagedropcalculator.businesslogic.voltagedrop.valueobjects.Length
 import com.cjanie.voltagedropcalculator.businesslogic.voltagedrop.valueobjects.Section
-import com.cjanie.voltagedropcalculator.businesslogic.voltagedrop.valueobjects.ConductorVoltageDropProperty
-import com.cjanie.voltagedropcalculator.businesslogic.voltagedrop.valueobjects.VoltageDrop
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 
-class VoltageDropTest {
+class VoltageDropCalculatorTest {
 
     @Test
     fun voltageDropAtEndOfLineLighting() {
@@ -19,15 +18,16 @@ class VoltageDropTest {
         // copper 2.5 mm2
         // I = 20 A
         val S = Section(inMillimeterSquare = 2.5f)
-        val K = ConductorVoltageDropProperty.property(
-            conductorMaterial = ConductorMaterial.COPPER,
-            section = S,
-            usage = Usage.LIGHTING
-        )
         val I = Intensity(inAmpere = 20f)
         val L = Length(inKilometer = 0.02f)
 
-        val delta_U = VoltageDrop.voltageDropAtEndOfLine(K, I, L)
+        val delta_U = VoltageDropCalculator(
+            conductorMaterial = ConductorMaterial.COPPER,
+            S = S,
+            usage = Usage.LIGHTING,
+            I = I,
+            L = L
+        ).voltageDropAtEndOfLine
 
         assertEquals(7.584f, delta_U.inVolt)
     }
